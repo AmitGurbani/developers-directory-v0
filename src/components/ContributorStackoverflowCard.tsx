894374b157951b77a7868f1f9ff9be1091c16278
Amit Gurbani <amit1994.gurbani@gmail.com>
2023-04-15 19:54:15 +0530

    Added more SO data

--- src/components/ContributorStackoverflowCard.tsx
+++ src/components/ContributorStackoverflowCard.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from "react";
 import useStackoverflow from "../hooks/useStackoveflow";
 import { FaMedal } from "@react-icons/all-files/fa/FaMedal";
+import { FaStackOverflow } from "@react-icons/all-files/fa/FaStackOverflow";
+import { BsArrowRightShort } from "@react-icons/all-files/bs/BsArrowRightShort";
 
 type ContributorStackoverflowCardProps = {
   stackoverflow: string;
@@ -9,63 +11,70 @@ type ContributorStackoverflowCardProps = {
 const ContributorStackoverflowCard: FunctionComponent<
   ContributorStackoverflowCardProps
 > = ({ stackoverflow }) => {
-  const { userData } = useStackoverflow(stackoverflow);
+  const { userData, userBadges, userTopTags } = useStackoverflow(stackoverflow);
 
   return (
-    <div>
-      {/* {userData?.display_name} */}
-
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src="/docs/images/blog/image-1.jpg"
-            alt=""
-          />
-        </a>
-        <div className="p-5">
-          <a href="#">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href={userData?.link} target="_blank">
+        <img className="rounded-t-lg" src={userData?.profile_image} alt="" />
+      </a>
+      <div className="p-5">
+        <a href={userData?.link} target="_blank">
+          <div className="flex">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {userData?.display_name}
             </h5>
-          </a>
+          </div>
+        </a>
 
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Reputation
-          </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Reputation - {userData?.reputation}
+        </p>
+        <div className="flex">
+          <div className="flex mb-3 font-normal text-amber-400 mr-4">
+            <FaMedal size={16} className="mt-1" /> {userData?.badge_counts.gold}
+          </div>
 
-          <p className="mb-3 font-normal text-amber-400 ">
+          <div className="flex mb-3 font-normal text-slate-400 mr-4">
             <FaMedal size={16} className="mt-1" />
-          </p>
+            {userData?.badge_counts.silver}
+          </div>
 
-          <p className="mb-3 font-normal text-orange-400 ">
+          <div className="flex mb-3 font-normal text-orange-400 ">
             <FaMedal size={16} className="mt-1" />
-          </p>
-
-          <p className="mb-3 font-normal text-slate-400 ">
-            <FaMedal size={16} className="mt-1" />
-          </p>
-
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
+            {userData?.badge_counts.bronze}
+          </div>
         </div>
+
+        <div>
+          <h1>Badges earned - { userBadges.length }</h1>
+          {userBadges.map(({ name, link }) => (
+            <a href={link} target="_blank">
+              <span className="mr-2 px-2 bg-orange-200 rounded-full">
+                {name}
+              </span>
+            </a>
+          ))}
+        </div>
+
+        <div>
+          <h1>Top tags - { userTopTags.length }</h1>
+          {userTopTags.map((tag) => (
+              <span className="mr-2 px-2 bg-orange-200 rounded-full">
+                {tag}
+              </span>
+          ))}
+        </div>
+
+        <a
+          href={userData?.link}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          target="_blank"
+        >
+          <FaStackOverflow size={24} />
+          <span className="mx-1">{userData?.display_name}</span>
+          <BsArrowRightShort size={24} />
+        </a>
       </div>
     </div>
   );
--- src/hooks/useStackoveflow.ts
+++ src/hooks/useStackoveflow.ts
@@ -7,19 +7,32 @@ type StackoverflowUserData = {
     gold: number;
     silver: number;
   };
+  creation_date: number;
   display_name: string;
   link: string;
+  profile_image: string;
   reputation: number;
+};
+
+type StackoverflowUserBadge = {
+  name: string;
+  link: string;
 };
 
 const BASE_URL = "https://api.stackexchange.com/2.3";
 
 const useStackoverflow = (userId: string) => {
+  const USER_BASE_URL = `${BASE_URL}/users/${userId}`;
   const [userData, setUserData] = useState<StackoverflowUserData | null>(null);
+  const [userBadges, setUserBadges] = useState<Array<StackoverflowUserBadge>>(
+    []
+  );
+
+  const [userTopTags, setUserTopTags] = useState<Array<string>>([]);
 
   const getUserData = async () => {
     try {
-      const response = await axios.get(`${BASE_URL}/users/${userId}`, {
+      const response = await axios.get(USER_BASE_URL, {
         params: {
           key: process.env.GATSBY_SO_KEY,
           site: "stackoverflow",
@@ -36,11 +49,60 @@ const useStackoverflow = (userId: string) => {
     }
   };
 
+  const getUserBadges = async () => {
+    try {
+      const response = await axios.get(`${USER_BASE_URL}/badges`, {
+        params: {
+          key: process.env.GATSBY_SO_KEY,
+          site: "stackoverflow",
+        },
+      });
+      const badges: Array<any> = response.data.items;
+      if (badges) {
+        setUserBadges(
+          badges.map(({ name, link }) => {
+            return {
+              link,
+              name,
+            };
+          })
+        );
+      } else {
+        console.log("User badges not found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUserTopTags = async () => {
+    try {
+      const response = await axios.get(`${USER_BASE_URL}/top-tags`, {
+        params: {
+          key: process.env.GATSBY_SO_KEY,
+          site: "stackoverflow",
+        },
+      });
+      const tags: Array<any> = response.data.items;
+      if (tags) {
+        setUserTopTags(
+          tags.map(({ tag_name }) => tag_name)
+        );
+      } else {
+        console.log("User top tags not found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getUserData();
+    getUserBadges();
+    getUserTopTags();
   }, [userId]);
 
-  return { userData };
+  return { userData, userBadges, userTopTags };
 };
 
 export default useStackoverflow;
