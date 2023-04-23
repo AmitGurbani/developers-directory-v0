c9b0d9ebfc587c166dda2e1076540fcb6d838ce4
Vishal Tailor <vdtailor@gmail.com>
2023-04-23 18:21:34 -0400

    Update Github Card

--- src/components/ContributorGithubCard.tsx
+++ src/components/ContributorGithubCard.tsx
@@ -31,7 +31,7 @@ const ContributorGithubCard: FunctionComponent<ContributorGithubCardProps> = ({
   } = github;
   return (
     <div>
-      <div className="max-w-sm bg-white text-gray-900 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:text-white">
+      {/* <div className="max-w-sm bg-white text-gray-900 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:text-white">
         <a href={url} target="_blank">
           <img className="rounded-t-lg w-full" src={avatarUrl} alt={name} />
         </a>
@@ -91,7 +91,76 @@ const ContributorGithubCard: FunctionComponent<ContributorGithubCardProps> = ({
             <BsArrowRightShort size={24} />
           </a>
         </div>
-      </div>
+          </div> */}
+
+      {/* Horizontal Card */}
+
+      <a
+        href={url}
+        target="_blank"
+        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <img
+          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src={avatarUrl}
+          alt={name}
+        />
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {login}
+          </h5>
+          <span className="mt-1 ml-2 text-gray-900 dark:text-white">
+            (since {new Date(createdAt).getFullYear()})
+          </span>
+
+          <div className="flex mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <HiOutlineUsers size={16} className="mt-1 mr-1" />{" "}
+            {followers.totalCount} followers &#x2022; {following.totalCount}{" "}
+            following
+          </div>
+
+          {contributionsCollection.hasAnyContributions && (
+            <div className="font-normal text-gray-700 dark:text-gray-400">
+              <div className="flex ">
+                <GoGitBranch size={16} className="mt-1 mr-1 flex" />{" "}
+                <h6 className="text-xl">Contributions - </h6>{" "}
+              </div>
+              <p className="flex">
+                <GoGitCommit size={16} className="mt-1 mr-1" /> Commits:{" "}
+                {contributionsCollection.totalCommitContributions}
+              </p>
+              <p className="flex">
+                <GoIssueOpened size={16} className="mt-1 mr-1" /> Issues:{" "}
+                {contributionsCollection.totalIssueContributions}
+              </p>
+              <p className="flex">
+                <VscGitPullRequest size={16} className="mt-1 mr-1" /> Pull
+                Requests:{" "}
+                {contributionsCollection.totalPullRequestContributions}
+              </p>
+              <p className="flex">
+                <BiGitCompare size={16} className="mt-1 mr-1" />
+                Pull Request Reviews:{" "}
+                {contributionsCollection.totalPullRequestReviewContributions}
+              </p>
+              <p className="flex">
+                <GoRepo size={16} className="mt-1 mr-1" /> Repositories:{" "}
+                {contributionsCollection.totalRepositoryContributions}
+              </p>
+            </div>
+          )}
+
+          <a
+            href={url}
+            className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            target="_blank"
+          >
+            <FaGithub size={24} />
+            <span className="mx-1">{login}</span>
+            <BsArrowRightShort size={24} />
+          </a>
+        </div>
+      </a>
     </div>
   );
 };
